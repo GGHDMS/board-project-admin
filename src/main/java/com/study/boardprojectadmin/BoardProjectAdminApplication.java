bc988dbad660d841043c19d0a335e4a437de3e12
HeoSeokMun <tjrans303@soongsil.ac.kr>
2023-05-29 23:56:19 +0900

    #4 - 리팩토링: 들여쓰기를 탭에서 스페이스로 변경
    자동 생성 코드는 기본값이 탭인 모양이다.
    들여쓰기는 스페이스를 쓰고 싶으므로, 변경해준다.

--- src/main/java/com/study/boardprojectadmin/BoardProjectAdminApplication.java
+++ src/main/java/com/study/boardprojectadmin/BoardProjectAdminApplication.java
@@ -8,8 +8,8 @@ import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 @SpringBootApplication
 public class BoardProjectAdminApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BoardProjectAdminApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(BoardProjectAdminApplication.class, args);
+    }
 
 }
