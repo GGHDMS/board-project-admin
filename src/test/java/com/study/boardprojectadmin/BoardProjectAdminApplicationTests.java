b5f8b57f6f54a047ce3794815dccd07d8e1aac2e
HeoSeokMun <tjrans303@soongsil.ac.kr>
2023-05-29 23:48:25 +0900

    #4 - 스프링 부트 프로젝트 초기화
    #2 에서 분석했던 내용을 토대로
    스프링 부트 뼈대가 되는 코드를 작성
    개발준비 완료

---
+++ src/test/java/com/study/boardprojectadmin/BoardProjectAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.boardprojectadmin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardProjectAdminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
