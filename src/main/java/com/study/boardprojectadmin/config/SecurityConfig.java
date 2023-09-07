95e4af7307056eb83b189775ba27f56d03640c6c
HeoSeokMun <tjrans303@soongsil.ac.kr>
2023-09-07 15:51:18 +0900

    #17 - SecurityConfig 업데이트: 인증 설정 추가
    * 인증 규칙을 좀 더 세밀하게 설정, 모든 접근은 이제 인증을 필요로 함
    * 카카오 인증을 위한 준비
    * PasswordEncoder 등록

--- src/main/java/com/study/boardprojectadmin/config/SecurityConfig.java
+++ src/main/java/com/study/boardprojectadmin/config/SecurityConfig.java
@@ -1,9 +1,26 @@
 package com.study.boardprojectadmin.config;
 
+import com.study.boardprojectadmin.domain.constant.RoleType;
+import com.study.boardprojectadmin.dto.security.BoardAdminPrincipal;
+import com.study.boardprojectadmin.dto.security.KakaoOAuth2Response;
+import com.study.boardprojectadmin.service.AdminAccountService;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.security.web.SecurityFilterChain;
+
+import java.util.Set;
+import java.util.UUID;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
@@ -12,12 +29,80 @@ import static org.springframework.security.config.Customizer.withDefaults;
 public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        String[] rolesAboveManger = {RoleType.MANAGER.name(), RoleType.DEVELOPER.name(), RoleType.ADMIN.name()};
+
         return http.csrf().disable()
-                .authorizeHttpRequests(auth -> auth.anyRequest().permitAll())
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
+                        .mvcMatchers(HttpMethod.POST, "/**").hasAnyRole(rolesAboveManger)
+                        .mvcMatchers(HttpMethod.DELETE, "/**").hasAnyRole(rolesAboveManger)
+                        .anyRequest().authenticated()
+                )
                 .formLogin(withDefaults())
                 .logout(logout -> logout.logoutSuccessUrl("/"))
                 .oauth2Login(withDefaults())
                 .build();
     }
+
+    @Bean
+    public UserDetailsService userDetailsService(AdminAccountService adminAccountService) {
+        return username -> adminAccountService
+                .searchUser(username)
+                .map(BoardAdminPrincipal::from)
+                .orElseThrow(() -> new UsernameNotFoundException("유저를 찾을 수 없습니다 - username: " + username));
+    }
+
+    /**
+     * <p>
+     * OAuth 2.0 기술을 이용한 인증 정보를 처리한다.
+     * 카카오 인증 방식을 선택.
+     *
+     * <p>
+     * TODO: 카카오 도메인에 결합되어 있는 코드. 확장을 고려하면 별도 인증 처리 서비스 클래스로 분리하는 것이 좋지만, 현재 다른 OAuth 인증 플랫폼을 사용할 예정이 없어 이렇게 마무리한다.
+     *
+     * @param adminAccountService  게시판 서비스의 사용자 계정을 다루는 서비스 로직
+     * @param passwordEncoder 패스워드 암호화 도구
+     * @return {@link OAuth2UserService} OAuth2 인증 사용자 정보를 읽어들이고 처리하는 서비스 인스턴스 반환
+     */
+    @Bean  // OAuth2UserService 는 interface
+    public OAuth2UserService<OAuth2UserRequest, OAuth2User> oAuth2UserService(
+            AdminAccountService adminAccountService,
+            PasswordEncoder passwordEncoder
+    ) {
+        final DefaultOAuth2UserService delegate = new DefaultOAuth2UserService();
+
+        return userRequest -> { // userRequest lambda 식의 입력이다.
+            OAuth2User oAuth2User = delegate.loadUser(userRequest); // OAuth2UserService 를 사용하려고 할 때 구현 해줘야하는 메소드 DefaultOAuth2UserService 에 구현되어 있다.
+
+            // 아래는 모두 kakao 응답에 맞춰 parsing을 해서 내보내는 것이다
+            KakaoOAuth2Response kakaoOAuth2Response = KakaoOAuth2Response.from(oAuth2User.getAttributes());
+            String registrationId = userRequest.getClientRegistration().getRegistrationId(); // 고유 값 "kakao" yml 파일에서 작성 해준 값
+            String providerId = String.valueOf(kakaoOAuth2Response.getId());
+            String username = registrationId + "_" + providerId; // kakaoOAuth2Response 에서는 사용할수 있는 값이 없다.
+            String dummyPassword = passwordEncoder.encode("{bcrypt}" + UUID.randomUUID()); // kakao를 통해 로그인 하기 때문에 pw는 사실 필요 없지만 dp 설계가 null 이 안되니 때문에 만들어 넣어준다.
+            Set<RoleType> roleTypes = Set.of(RoleType.USER);
+
+            return adminAccountService.searchUser(username)
+                    .map(BoardAdminPrincipal::from)
+                    .orElseGet(() -> // db 에 있었으면 그대로 진행 없으면 아래와 같이 db에 만들어 준다.
+                            BoardAdminPrincipal.from(
+                                    adminAccountService.saveUser(
+                                            username,
+                                            dummyPassword,
+                                            roleTypes,
+                                            kakaoOAuth2Response.email(),
+                                            kakaoOAuth2Response.nickname(),
+                                            null
+                                    )
+                            )
+                    );
+        };
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
 
 }
